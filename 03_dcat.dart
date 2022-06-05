52ea40892f3d4ec098c19194362a74f194f9d282
naivvve <1532619822@qq.com>
2022-06-05 20:27:51 +0800

    Message

--- 03_dcat.dart
+++ 03_dcat.dart
@@ -1,8 +1,10 @@
-
 // 命令行程序 （https://dart.dev/tutorials/server/cmdline）
+
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:args/args.dart';
+
 const lineNumber = 'line-number';
 
 void main(List<String> args) {
@@ -11,18 +13,17 @@ void main(List<String> args) {
 
   ArgResults argResults = parser.parse(args);
 
-  final paths = argResults.rest; 
+  final paths = argResults.rest; // 剩余的命令行参数
 
   dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
 }
-
 
 Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
   if (paths.isEmpty) {
     print('type exit to quit.');
     while (true) {
       stdout.write(
-          '> '); 
+          '> '); // 这样就不换行了 (https://stackoverflow.com/questions/14073217/print-without-a-newline-in-dart)
       String? line = stdin.readLineSync();
       print('${line}\n');
 
